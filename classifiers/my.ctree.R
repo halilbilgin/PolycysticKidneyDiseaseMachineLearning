4734b9547ea1b66d33c447c4048c0a7241ff578b
Halil BİLGİN <Halil BLGN>
2017-03-30 08:47:53 +0300

    Change ctree_control

--- classifiers/my.ctree.R
+++ classifiers/my.ctree.R
@@ -1,8 +1,8 @@
 function(trData, fm, 
-         tuneGrid = expand.grid(.mincriterion = 0.99, 
+         tuneGrid = expand.grid(.mincriterion = 0.95, 
                                             .maxdepth = as.integer(seq(5, 20, 2))),
          controls = ctree_control(teststat = 'max', 
-                                  testtype='Teststatistic'), method='ctree2', ...) {
+                                  minsplit=5), method='ctree2', ...) {
   fit1 <- train(fm, data = trData, 
                 method = 'ctree2', 
                 tuneGrid = tuneGrid, controls = controls, ...)
