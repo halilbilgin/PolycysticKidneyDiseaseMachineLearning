cae0e9c3e975faee383c1a6f3db0012eb342cd7e
Halil Bilgin <Halil Bilgin>
2017-03-31 18:45:40 +0300

    Remove only the outliers which are likely to be miswritten

--- manipulations/getProcessedData.R
+++ manipulations/getProcessedData.R
@@ -1,16 +1,19 @@
 library(missForest)
-function(dataraw) {
-  dataraw[, c('gfrloss', 'gfr', 'CVolay', 'gozlemno', 'diyaliz', 'diyaliz_sure')] <-NULL
-  dataraw <- dataraw[dataraw[, 'crp'] < 150,] 
-
+function(dataraw, delOutliers=T) {
+  dataraw[, c('gfrloss', 'gozlemno', 'gfr', 'CVolay', 'diyaliz', 'diyaliz_sure')] <-NULL
+  
   dataimp = missForest(dataraw)$ximp
   dataimp[, 'eGFR'] <- 186 * (dataimp$kreatinin / 88.4)^(-1.154) * (dataimp$yas)^(-0.203) * ifelse(dataimp[,'cinsiyet'] == 'kadın', 0.742, 1) 
-  for(nm in names(dataimp)) {
-    if(is.numeric(dataimp[,nm])) {
-      dataimp <- manipulations$deleteOutliers(dataimp, nm) 
-    }
+  
+  if(delOutliers) {
+    dataimp <- manipulations$deleteOutliers(dataimp, 'LDL') 
+    dataimp <- manipulations$deleteOutliers(dataimp, 'kolesterol')
+    
+    dataimp[,'crp'] <- ifelse(dataimp[,'crp'] > 150, NA, dataimp[,'crp'])
+
+    dataimp <- missForest(dataimp)$ximp  
   }
-  dataimp <- missForest(dataimp)$ximp
+  
   
   dataimp[, 'cinsiyet'] <- ifelse(dataimp[,'cinsiyet'] == 'erkek', 0, 1)
   dataimp[, 'ht'] <- ifelse(dataimp[, 'ht'] == 'var', 1, 0)
