4ba438bcbb83d34c9e944acf80e91c8d45a68fb1
Halil Bilgin <Halil Bilgin>
2017-03-31 18:57:31 +0300

    Don't print iterations

--- featureSelection/aic.R
+++ featureSelection/aic.R
@@ -1,9 +1,9 @@
 function(scopes, data) {
   features <- list()
   fit <- glm(as.factor(gfrloss2)~1, data=db, family=binomial, control=glm.control(maxit=10000))
-  
+
   for(scope in scopes) {
-    step <- stepAIC(fit, scope, direction="both", steps = 1000)
+    step <- stepAIC(fit, scope, direction="both", steps = 1000, trace=F)
     features <- append(features, list(step$formula))
   }
  
