1e25bf16cf8f9084761c0f638347c7242854b1d3
Halil Bilgin <Halil Bilgin>
2017-04-03 12:32:02 +0300

    Add lasso method

---
+++ featureSelection/lasso.R
@@ -0,0 +1,20 @@
+function(scopes, data) {
+  features <- list()
+  library(glmnet, verbose = F)
+  set.seed(seed)
+
+  
+  return(sapply(scopes, function(scope) {
+    MSEs <- NULL
+    
+    cv <- cv.glmnet(model.matrix(gfrloss2~., scaledDb)[,-1], scaledDb$gfrloss2, 
+                    family='binomial', alpha=1, nfolds=nrow(scaledDb))
+    
+    coefs <- as.matrix(predict(cv, type='coefficients', s=cv$lambda.min))
+    coefs <- coefs[coefs[,'1'] != 0,]
+    coefs <- names(coefs)[names(coefs) != '(Intercept)']
+    
+    return(as.formula(paste('as.factor(gfrloss2) ~ ', paste(coefs, collapse = '+'))))
+    }))
+  
+}
