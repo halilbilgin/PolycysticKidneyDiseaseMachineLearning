3f8e44e7ebdb965028222ac9f41c119f2e6279a7
Halil Bilgin <Halil Bilgin>
2017-04-03 14:05:02 +0300

    Convert to LOOCV and Add a constant if lasso results in 0 variable

--- featureSelection/lasso.R
+++ featureSelection/lasso.R
@@ -7,14 +7,16 @@ function(scopes, data) {
   return(sapply(scopes, function(scope) {
     MSEs <- NULL
     
-    cv <- cv.glmnet(model.matrix(gfrloss2~., scaledDb)[,-1], scaledDb$gfrloss2, 
-                    family='binomial', alpha=1, nfolds=5)
+    cv <- cv.glmnet(model.matrix(gfrloss2~., data)[,-1], data$gfrloss2, 
+                    family='binomial', alpha=1, nfolds=nrow(data))
     
     coefs <- as.matrix(predict(cv, type='coefficients', s=cv$lambda.min))
     coefs <- coefs[coefs[,'1'] != 0,]
     coefs <- names(coefs)[names(coefs) != '(Intercept)']
-    
-    return(as.formula(paste('as.factor(gfrloss2) ~ ', paste(coefs, collapse = '+'))))
+    if(length(coefs) == 0){
+      coefs <- c(1)
+    }
+    return(as.formula(paste("as.factor(gfrloss2) ~ ", paste(coefs, collapse = '+'))))
     }))
   
 }
