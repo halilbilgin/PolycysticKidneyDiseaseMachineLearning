e5e12a7046b1496e912018444770446692e7504b
Halil Bilgin <Halil Bilgin>
2017-04-07 12:46:59 +0300

    Fix bug

--- manipulations/scaleDb.R
+++ manipulations/scaleDb.R
@@ -1,9 +1,9 @@
 function(dataset) {
   return(as.data.frame(sapply(colnames(dataset), function(x) {
-    if(is.numeric(db[, x]) && !(x %in% c('ht', 'cinsiyet','gfrloss2'))) {
-      output <- scale(db[, x], center = TRUE, scale = TRUE)
+    if(is.numeric(dataset[, x]) && !(x %in% c('ht', 'cinsiyet','gfrloss2'))) {
+      output <- scale(dataset[, x], center = TRUE, scale = TRUE)
     } else {
-      output <- db[, x]
+      output <- dataset[, x]
     }
     return(output)
     
