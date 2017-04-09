f6715d1616a65925a222f94630889b4d491defeb
Halil Bilgin <Halil Bilgin>
2017-04-10 02:18:05 +0300

    Make it more generalised function

--- featureSelection/aic.R
+++ featureSelection/aic.R
@@ -1,13 +1,15 @@
-function(scopes, data) {
+function(scopes, data, starting='as.factor(gfrloss2)~ht+proteinuri',
+         direction='both', steps=5) {
   features <- list()
+  starting <- as.formula(starting)
   # Proteinuri is related to decline in GFR : https://www.researchgate.net/profile/Shigeko_Hara/publication/5353745_Slower_Decline_of_Glomerular_Filtration_Rate_in_the_Japanese_General_Population_A_Longitudinal_10-Year_Follow-Up_Study/links/53eccd530cf2981ada10eb45/Slower-Decline-of-Glomerular-Filtration-Rate-in-the-Japanese-General-Population-A-Longitudinal-10-Year-Follow-Up-Study.pdf
-  fit <- glm(as.factor(gfrloss2)~proteinuri, data=db, family=binomial, control=glm.control(maxit=10000))
-
+  fit <- glm(starting, data=data, family=binomial, control=glm.control(maxit=10000))
+  
   for(scope in scopes) {
-    step <- stepAIC(fit, scope, direction="both", steps = 100, trace=F)
+  step <- stepAIC(fit, scope, direction=direction, steps = steps, trace=F, k=2)
     features <- append(features, list(step$formula))
   }
- 
+  
   names(features) <- names(scopes)
   
   return(features)
